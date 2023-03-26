1f237cbd07fd453d585bd8cb88c21e43d9554037
Jorge Leon <84067777+jorge2751@users.noreply.github.com>
2023-03-26 13:34:35 -0700

    Practicing with Material UI

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
+import {
+  Paper,
+  FormControl,
+  InputLabel,
+  OutlinedInput,
+  Button
+} from '@mui/material'
 import './App.css';
 
 function App() {
+  const styles = {
+    paper: {
+        width: "20rem", padding: "1rem"
+    },
+    input: {
+        marginBottom: "1rem"
+    },
+    button: {
+        width: "100%"
+    }
+}
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <Paper elevation={3} style={styles.paper}>
+        <h2>Login Form</h2>
+        <form>
+            <FormControl variant="outlined" style={styles.input}>
+                <InputLabel>Username</InputLabel>
+                <OutlinedInput type="text"/>
+            </FormControl>
+            <FormControl variant="outlined" style={styles.input}>
+                <InputLabel>E-mail</InputLabel>
+                <OutlinedInput type="email"/>
+            </FormControl>
+            <FormControl variant="outlined" style={styles.input}>
+                <InputLabel>Password</InputLabel>
+                <OutlinedInput type="password"/>
+            </FormControl>
+            <FormControl variant="outlined" style={styles.input}>
+                <InputLabel>Password</InputLabel>
+                <OutlinedInput type="password"/>
+            </FormControl>
+            <Button type="submit" variant="contained" color="primary">
+                Register
+            </Button>
+        </form>
+    </Paper>
+)
 }
 
 export default App;
